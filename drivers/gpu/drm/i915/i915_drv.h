7c8a6243ae6459e6b7ab579ce69941dc244cd283
Jan-Simon Möller <dl9pf@gmx.de>
2012-11-14 13:03:57 +0100

    Fix statement in i915 driver.
    Author:  PaX Team <pageexec at freemail.hu>
    ML-Post: http://lists.cs.uiuc.edu/pipermail/llvm-commits/Week-of-Mon-20120507/142707.html
    URL:     http://llvm.linuxfoundation.org
    
    Merge:   Jan-Simon Möller <dl9pf at gmx.de>
    
    Description:

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1549,7 +1549,7 @@ void i915_teardown_sysfs(struct drm_device *dev_priv);
 /* intel_i2c.c */
 extern int intel_setup_gmbus(struct drm_device *dev);
 extern void intel_teardown_gmbus(struct drm_device *dev);
-extern inline bool intel_gmbus_is_port_valid(unsigned port)
+static inline bool intel_gmbus_is_port_valid(unsigned port)
 {
 	return (port >= GMBUS_PORT_SSC && port <= GMBUS_PORT_DPD);
 }
