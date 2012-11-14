3be20f85c300b5c07c0e6a1cc820a7e57e8b6486
Jan-Simon Möller <dl9pf@gmx.de>
2012-11-14 13:12:21 +0100

    Fix braces for compilation with clang
    Author:  PaX Team <pageexec at freemail.hu>
    ML-Post: http://lists.cs.uiuc.edu/pipermail/llvm-commits/Week-of-Mon-20120507/142707.html
    URL:     http://llvm.linuxfoundation.org
    
    Merge:   Jan-Simon Möller <dl9pf at gmx.de>
    
    Description:

--- drivers/vhost/vhost.c
+++ drivers/vhost/vhost.c
@@ -425,7 +425,7 @@ int vhost_zerocopy_signal_used(struct vhost_virtqueue *vq)
 	int j = 0;
 
 	for (i = vq->done_idx; i != vq->upend_idx; i = (i + 1) % UIO_MAXIOV) {
-		if ((vq->heads[i].len == VHOST_DMA_DONE_LEN)) {
+		if (vq->heads[i].len == VHOST_DMA_DONE_LEN) {
 			vq->heads[i].len = VHOST_DMA_CLEAR_LEN;
 			vhost_add_used_and_signal(vq->dev, vq,
 						  vq->heads[i].id, 0);
